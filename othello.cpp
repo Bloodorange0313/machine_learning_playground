a1fd365f95dd42d43d1d495c843b623c08b0562c
Bloodorange0313 <koudai7272@gmail.com>
2019-02-23 23:52:45 +0900

    盤面の完成

--- othello.cpp
+++ othello.cpp
@@ -9,7 +9,9 @@ int white = 0;
 int black = 1;
 int board_size = 8;
 int cells[8][8];
+int index1, index2;
 
+//ボードを初期化する
 int board_initialize(int i, int j){
   //int cells[board_size][board_size];
   if(i == 3 && j == 3){
@@ -30,52 +32,7 @@ int board_initialize(int i, int j){
   
   return cells[i][j];
 }
-/*
-bool list_flippable_disk(int x, int y, int player){
-  int prev = -1;
-  int next = 1;
-  int direction[3] = {prev, 0, next};
-  vector<pair<int, int> >flippable;
-  for(int dx = 0; dx < 3; ++dx){
-    for(int dy = 0; dy < 3; ++dy){
-      if(dx == 0 && dy == 0){
-        continue;
-      }
-      vector<pair<int, int>>tmp;
-      int depth = 0;
-      while(true){
-        depth++;
-        int rx = x + (dx * depth);
-        int ry = y + (dy * depth);
-        if(0 <= rx && rx < board_size && 0 <= ry && ry < board_size){
-          int request = cells[rx][ry];
-          if(request == -1){
-            break;
-          }
-          if(request == player){
-            if(!tmp.empty()){
-              //flippableにtmpの石を追加したい
-              while(!tmp.empty()){
-                flippable.push_back(make_pair(tmp[0].first,tmp[0].second));
-              }
-            }
-          }
-          else{
-            tmp.push_back(make_pair(rx, ry));
-          }
-        }else{
-          break;
-        }
-      }
-    }
-  }
-  if(flippable.empty()){
-    return false;
-  }else{
-    return true;
-  }
-}
-*/
+
 //(x, y)に置いた時に、(s, t)をひっくり返すことができるか
 bool list_flippable_disk2(int x, int y, int s, int t, int player){
   int prev = -1;
@@ -94,7 +51,7 @@ bool list_flippable_disk2(int x, int y, int s, int t, int player){
         int rx = x + (direction[dx] * depth);
         int ry = y + (direction[dy] * depth);
         if(0 <= rx && rx < board_size && 0 <= ry && ry < board_size){
-          int request = cells[ry][rx];
+          int request = cells[rx][ry];
           if(request == -1){
             break;
           }
@@ -123,16 +80,18 @@ bool list_flippable_disk2(int x, int y, int s, int t, int player){
   }
   bool dd = false;
   for(int i = 0; i < flippable.size(); ++i){
-      if(flippable[i].first == t && flippable[i].second == s){
+      if(flippable[i].first == s && flippable[i].second == t){
         dd = true;
       }
   }
   return dd;
 }
 
+
+//石を置く関数
 bool put_disk(int x, int y, int player){
   //すでに石が置いてあると置くことができない
-  if(cells[y][x] != -1){
+  if(cells[x][y] != -1){
     return false;
   }
   //獲得できる石がないときも置くことができない
@@ -150,20 +109,22 @@ bool put_disk(int x, int y, int player){
   }
 
   //実際に石をおく処理
-  cells[y][x] = player;
+  cells[x][y] = player;
   for(int i = 0; i < board_size; ++i){
     for(int j = 0; j < board_size; ++j){
       if(list_flippable_disk2(x, y, i, j, player) != false){
 
-        cells[j][i] = player;
+        cells[i][j] = player;
+        /*
         //flippableに追加されたものを入れる必要がある。
         for(int s = 0; s < board_size; ++s){
           for(int t = 0; t < board_size; ++t){
             if(list_flippable_disk2(i, j, t, s, player) == true){
-              cells[y][x] = player;
+              cells[s][t] = player;
             }
           }
         }
+        */
         
       }
     }
@@ -172,22 +133,138 @@ bool put_disk(int x, int y, int player){
   
 }
 
+//オセロの盤面を表示する
+void show_board(){
+  cout << "--";
+  for(int i = 0; i < 8; ++i){
+    cout << i << "-";
+  }
+  cout << endl;
+  for(int i = 0; i < board_size; ++i){
+    for(int j = 0; j < board_size; ++j){
+      //cells[j][i] = board_initialize(i, j);
+    }
+  }
+  for(int i = 0; i < board_size; ++i){
+    cout << i << " ";
+    for(int j = 0; j < board_size; ++j){
+      //put_disk(i, j, player);
+      //cout << j << " ";
+      if(cells[i][j] == white){
+        cout << "W ";
+      }else if(cells[i][j] == black){
+        cout << "B ";
+      }else{
+        cout << "* ";
+      }
+    }
+    cout << endl;
+  }
+}
+
+int winner = -1;
 
 
+//白の石の個数を数える
+int get_white_disk(){
+  int cnt = 0;
+  for(int i = 0; i < board_size; ++i){
+    for(int j = 0; j < board_size; ++j){
+      if(cells[i][j] == 0){
+        cnt++;
+      }
+    }
+  }
+  return cnt;
+}
 
+//黒の石の個数を数える
+int get_black_disk(){
+  int cnt = 0;
+  for(int i = 0; i < board_size; ++i){
+    for(int j = 0; j < board_size; ++j){
+      if(cells[i][j] == 1){
+        cnt++;
+      }
+    }
+  }
+  return cnt;
+}
 
-//string show_board()
+//石を置く場所があるかの判定
+bool putable(int player){
+  bool judge = false;
+  
+  for(int i = 0; i < board_size; ++i){
+    for(int j = 0; j < board_size; ++j){
+      for(int x = 0; x < board_size; ++x){
+        for(int y = 0; y < board_size; ++y){
+          if(list_flippable_disk2(i, j, x, y, player) == true){
+            judge = true;
+          }
+        }
+      }
+    }
+  }
+  return true;
+}
+
+//ゲームの終了の判定
+bool game_finish_judge(int player){
+  bool judge = false;
+  if(get_black_disk() == 0 || get_white_disk() == 0){
+    judge = true;
+  }
+  if(putable(player) == false){
+    judge = true;
+  }
+  return judge;
+}
 
 int main(){
   //cout << board_initialize(3,3) << endl;
   for(int i = 0; i < board_size; ++i){
     for(int j = 0; j < board_size; ++j){
-      cout << board_initialize(i, j);
+      //cout << board_initialize(i, j);
       board_initialize(i, j); //cellsの初期化
       
     }
     cout << endl;
   }
+  
+  int player = 0;
+  while(!game_finish_judge(player)){
+    if(player==0){
+      cout << "白のターンです" << endl;
+    }else{
+      cout << "黒のターンです" << endl;
+    }
+    
+    show_board();
+    cout << "場所を選んでください" << endl;
+    cin >> index1 >> index2;
+    if(put_disk(index2, index1, player) != false){
+      //put_disk(index1, index2, player);
+      put_disk(index2, index1, player);
+      if(player==1){
+        player = 0;
+        //cout << "白のターンです" << endl;
+      }else{
+        player = 1;
+        //cout << "黒のターンです" << endl;
+      }
+    }
+    
+  }
+  if(get_white_disk > get_black_disk){
+    cout << "勝者は白です" << endl;
+  }else if(get_white_disk > get_black_disk){
+    cout << "勝者は黒です" << endl;
+  }else{
+    cout << "引き分けです" << endl;
+  }
+  /*
+  
   for(int i = 0; i < board_size; ++i){
     for(int j = 0; j < board_size; ++j){
       cout << list_flippable_disk2(3, 5,i,j, white); //<< endl;
@@ -197,6 +274,10 @@ int main(){
     cout << endl;
     
   }
-  cout << put_disk(3, 4, white) << endl;
+  cout << list_flippable_disk2(3, 5, 3, 4, white) << endl;;
+  //cout << put_disk(3, 4, white) << endl;
   cout << put_disk(3, 5, white) << endl;
+  //cout << put_disk(4, 5, black) << endl;
+  show_board();
+  */
 }
